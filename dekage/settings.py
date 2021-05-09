3b82dddf31d60048cd2d4ceda52cb8282a6ebe24
vincenttpham <57119859+vincenttpham@users.noreply.github.com>
2021-05-08 17:35:00 -0700

    Update settings.py

--- dekage/settings.py
+++ dekage/settings.py
@@ -30,7 +30,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'dekageshop@gmail.com'
-EMAIL_HOST_PASSWORD = 'dekageadmin69'
+EMAIL_HOST_PASSWORD = 'ifvknlaejsmbgpdr'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
